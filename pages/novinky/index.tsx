a9405fa0d76e6fa68844c369197100d727218172
Jozef Drábik <jozef.drabik@the-software.sk>
2022-06-10 10:36:34 +0200

    Menu and Footer changes

--- pages/novinky/index.tsx
+++ pages/novinky/index.tsx
@@ -1,5 +1,5 @@
 export default function NewsPage() {
-  return <h1 className="text-3xl underline">Hello world!</h1>;
+  return <h1 className="text-3xl ">Hello world!</h1>;
 }
 
 export async function getStaticProps() {
