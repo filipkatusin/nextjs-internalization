26bdd53db9346fe8f66a43d89d563743799355b8
Filip <filip.katusin@the-software.sk>
2022-08-08 10:01:08 +0200

    added redirect status

--- pages/api/preview.ts
+++ pages/api/preview.ts
@@ -1,8 +1,10 @@
-export default function handler(req, res) {
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.query.secret !== process.env.NEXT_PREVIEW_SECRET) {
     return res.status(401).json({ message: "Invalid token" });
   }
 
   res.setPreviewData({ data: "data" }, { maxAge: 60 * 60 });
-  res.redirect("/");
+  res.redirect(200, "/");
 }
