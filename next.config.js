d5a5a60c132b9aba8ac5b48f51fddcd612cb3d0d
Filip <filip.katusin@the-software.sk>
2022-08-19 13:08:27 +0200

    changes

--- next.config.js
+++ next.config.js
@@ -2,6 +2,21 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  i18n: {
+      locales: ['sk', 'cs'],
+      defaultLocale: 'sk',
+      localeDetection: false,
+      domains: [
+          {
+              domain: "test.sportzoo.sk",
+              defaultLocale: "sk",
+          },
+          {
+              domain: "test.sportzoo.cz",
+              defaultLocale: "cs",
+          },
+      ],
+  },
 }
 
 module.exports = nextConfig
